e5f0f75a34e0f72315dae1596ad263e35420663f
Manuel-TOSH\Manuel <manneljo_silva79@sapo.pt>
2016-09-12 17:07:33 +0100

    Testes6

--- src/testes/BibliotecaTest.java
+++ src/testes/BibliotecaTest.java
@@ -44,6 +44,7 @@ public void InserirLivro() throws Exception {
 		Assert.assertEquals("Romance",l2.getCategoria());
 		Assert.assertEquals("Muito Valor",l2.getValorizacao());
 		
+		livros.add(l2);
 	
 	}
 
